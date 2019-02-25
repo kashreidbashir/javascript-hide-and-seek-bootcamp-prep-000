b33fcae8558b6e11a39641335fe95fb4f7b68ba8
Kash Reid-Bashir <kash@reid-bashir.com>
2019-02-25 17:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+// The above function returns the first element that matches the selector
+
 
 function nestedTarget() {
   return document.getElementById("nested").querySelector(".target");
 }
+// returns the first element that matches the selector
+
 
 function increaseRankBy(n) {
   var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
   for(let i=0;i<lis.length;i++) {
     lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+// returns the most deeply nested child in #grand-node. This is the most complex function to reproduce.
+// However, the main example serves a similar function.
+// The for function runs to the deepest nested piece of information in the main node in a rising fashion (i.e. i++)
+// The final line returns the information and look at this syntax prior to the interview. It is not particulary complex
+// but unfamiliar.
   }
 }
 
@@ -21,3 +30,5 @@ function deepestChild() {
   }
 return test;
 }
+
+// Print this solution prior to the interview. 
